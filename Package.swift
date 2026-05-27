613a79c3d1b4c6b2a1496964fdf38c50943533ac
xuqisheng <876415513@qq.com>
2026-05-27 15:42:46 +0800

    支持 SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SwifterKnife",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "SwifterKnife",
+            targets: ["SwifterKnife"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
+    ],
+    targets: [
+        .target(
+            name: "SwifterKnife",
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit"),
+            ],
+            path: "SwifterKnife/Classes"
+        ),
+    ]
+)
